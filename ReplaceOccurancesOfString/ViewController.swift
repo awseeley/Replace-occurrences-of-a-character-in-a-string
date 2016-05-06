2d08bef3d0c6ed4171384b9377a0e0cb5081cc50
Andrew Seeley <awseeley@gmail.com>
2016-05-06 14:49:15 +1200

    Replace occurrences of a character in a string
    Replace occurrences of a character in a string

---
+++ ReplaceOccurancesOfString/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ReplaceOccurancesOfString
+//
+//  Created by Andrew Seeley on 6/05/2016.
+//  Copyright © 2016 Seemu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        let myString: String = "This is my string"
+        let newString = myString.stringByReplacingOccurrencesOfString(" ", withString: "+")
+        
+        print(myString)
+        print(newString)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
