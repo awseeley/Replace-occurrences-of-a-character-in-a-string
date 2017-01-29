91390a8772c78fb9042d5029d796483372520536
Andrew Seeley <awseeley@gmail.com>
2017-01-29 21:42:28 +1100

    Updated for Swift 3
    Updated for Swift 3

--- ReplaceOccurancesOfString/ViewController.swift
+++ ReplaceOccurancesOfString/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         let myString: String = "This is my string"
-        let newString = myString.stringByReplacingOccurrencesOfString(" ", withString: "+")
+        let newString = myString.replacingOccurrences(of: " ", with: "+")
         
         print(myString)
         print(newString)
